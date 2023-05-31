a3611723517adea7fdf04e5c29b06efcd6066117
Ace <acereevesantos0718@gmail.com>
2023-05-31 12:09:10 +0800

    adafg

--- app/models/assets.model.js
+++ app/models/assets.model.js
@@ -8,15 +8,15 @@ const assetsSchema = new mongoose.Schema({
   header: {
     asset_class: { type: String, trim: true, required: true },
     company_code: { type: mongoose.SchemaTypes.ObjectId, required: true, ref: 'companies' },
-    number_of_similar_assets: { type: Number, trim: true, required: true },
+    number_of_similar_assets: { type: Number, trim: true, required: false },
     class: { type: Number, trim: true, required: true },
   },
   general: {
     general_data: {
-      description: { type: String, trim: true, required: true },
+      description: { type: String, trim: true, required: false },
       asset_main_no: { type: String, trim: true, required: false },
-      acct_determination: { type: Number, required: true },
-      serial_number: { type: Number, trim: true, required: true },
+      acct_determination: { type: Number, required: false },
+      serial_number: { type: Number, trim: true, required: false },
       inventory_number: { type: Number, trim: true, required: false },
       quantity: { type: Number, trim: true, required: false },
       manage_historically: { type: Boolean, required: true },
@@ -24,7 +24,7 @@ const assetsSchema = new mongoose.Schema({
     inventory: {
       last_inventory_on: { type: Date, default: () => new Date(), required: false },
       inventory_note: { type: String, trim: true, required: false },
-      include_asset_in_inventory_list: { type: Boolean, required: true },
+      include_asset_in_inventory_list: { type: Boolean, required: false },
     },
     posting_information: {
       capitalized_on: { type: String, trim: true, required: false },
@@ -36,8 +36,8 @@ const assetsSchema = new mongoose.Schema({
   time_dependent: {
     interval: {
       cost_center: { type: mongoose.SchemaTypes.ObjectId, required: true, ref: 'cost_centers' },
-      plant: { type: Number, trim: true, required: true },
-      location: { type: String, trim: true, required: true },
+      plant: { type: Number, trim: true, required: false },
+      location: { type: String, trim: true, required: false },
       room: { type: String, trim: true, required: false },
       shift_factor: { type: String, trim: true, required: false },
     },
